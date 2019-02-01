31c5b89a4b1f5967a76490ceac4c273f81819d31
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-02-01 21:35:58 +0000

    background can now be changed by clicking one of 4 buttons

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,3 +17,45 @@ $("input[type='text']").keypress(function(event){
 	}
 }); 
 	
+$("button").click(function(){
+	if (this.id === "b1") {
+		$("body").css({
+			background: "#1D4350",
+			background: "-webkit-linear-gradient(to right, #A43931, #1D4350)", 
+			background: "linear-gradient(to right, #A43931, #1D4350)"
+		});
+
+		$("button").prop("disabled", false);
+		$(this).prop("disabled", true);
+
+
+	} else if (this.id === "b2"){
+		$("body").css({
+			background: "#3a6186",  // fallback for old browsers //
+			background: "-webkit-linear-gradient(to right, #89253e, #3a6186)",  // Chrome 10-25, Safari 5.1-6 //
+			background: "linear-gradient(to right, #89253e, #3a6186)"
+		});	
+		$("button").prop("disabled", false);
+		$(this).prop("disabled", true);
+
+	} else if (this.id === "b3"){
+		$("body").css({
+			background: "#2C3E50",  
+			background: "-webkit-linear-gradient(to right, #FD746C, #2C3E50)",
+			background: "linear-gradient(to right, #FD746C, #2C3E50)" 
+		});
+		$("button").prop("disabled", false);
+		$(this).prop("disabled", true);
+
+	}else if (this.id === "b4"){
+		$("body").css({
+			background: "#43C6AC",  // fallback for old browsers //
+			background: "-webkit-linear-gradient(to right, #191654, #43C6AC)",  // Chrome 10-25, Safari 5.1-6 //
+			background: "linear-gradient(to right, #191654, #43C6AC)", // W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ //
+
+		});
+		$("button").prop("disabled", false);
+		$(this).prop("disabled", true);
+
+	}
+})
