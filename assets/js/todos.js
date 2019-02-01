4b3cb1dc828632e9d3f1ef1bdc9f1e3adeaf3d83
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-02-01 20:49:39 +0000

    added css to style container

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,19 @@
-$("li").click(function(){
+$("#toDoItems").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //click x to remove item
-$("span").click(function(event){
+$("#toDoItems").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	})
 	event.stopPropagation;
-}) 
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var text = $(this).val();
+		$("#toDoItems").append("<li> <span>x</span> " + text + "</li>");
+	}
+}); 
 	
